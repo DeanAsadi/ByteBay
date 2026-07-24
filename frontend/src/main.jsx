0ee4f49a675f79fe78af98edf4926f284d9efab2
Dean Asadi <eng.de86@gmail.com>
2026-07-23 17:11:56 -0700

    Set up Redux Toolkit store and cart slice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,20 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import App from './App.jsx';
+import store from './store.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import App from './App';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-
-    <BrowserRouter>
-
-      <App />
-
-    </BrowserRouter>
-
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,20 +1,105 @@
 import { useParams, useSearchParams } from 'react-router-dom';
+import products from '../data/products';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Card,
+} from 'react-bootstrap';
 
 const CartScreen = () => {
   const { id } = useParams();
   const [searchParams] = useSearchParams();
 
   const qty = Number(searchParams.get('qty')) || 1;
+  const product = products.find(
+  (product) => product.id === Number(id)
+);
 
+if (!product) {
+  return <h2>Product Not Found</h2>;
+}
+    
   return (
-    <>
-      <h1>Shopping Cart</h1>
+  <>
+    <h1>Shopping Cart</h1>
 
-      <p>Product ID: {id}</p>
+    <Row>
 
-      <p>Quantity: {qty}</p>
-    </>
-  );
+      <Col md={8}>
+
+        <ListGroup variant="flush">
+
+          <ListGroup.Item>
+
+            <Row>
+
+              <Col md={2}>
+
+                <Image
+                  src={product.image}
+                  alt={product.name}
+                  fluid
+                  rounded
+                />
+
+              </Col>
+
+              <Col md={4}>
+
+                {product.name}
+
+              </Col>
+
+              <Col md={2}>
+
+                ${product.price}
+
+              </Col>
+
+              <Col md={2}>
+
+                Qty: {qty}
+
+              </Col>
+
+            </Row>
+
+          </ListGroup.Item>
+
+        </ListGroup>
+
+      </Col>
+
+      <Col md={4}>
+
+        <Card>
+
+          <ListGroup variant="flush">
+
+            <ListGroup.Item>
+
+              <h4>
+
+                Subtotal
+
+              </h4>
+
+              ${product.price * qty}
+
+            </ListGroup.Item>
+
+          </ListGroup>
+
+        </Card>
+
+      </Col>
+
+    </Row>
+
+  </>
+);
 };
 
 export default CartScreen; 
