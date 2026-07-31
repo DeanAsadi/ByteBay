b0b312d5d757ecc5fd33b343a84c7470139e752e
Dean Asadi <eng.de86@gmail.com>
2026-07-31 10:34:30 -0700

    Display cart item count in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,46 +1,66 @@
+import { useSelector } from 'react-redux';
 import {
   Navbar,
   Nav,
-  Container
+  Container,
+  Badge,
 } from 'react-bootstrap';
-
 import {
   FaShoppingCart,
-  FaUser
+  FaUser,
 } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const cartItems = useSelector(
+    (state) => state.cart.cartItems
+  );
+
+  const cartItemsCount = cartItems.reduce(
+    (total, item) => total + item.qty,
+    0
+  );
+
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar
+        bg="dark"
+        variant="dark"
+        expand="lg"
+        collapseOnSelect
+      >
         <Container>
-
-          <Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
             ByteBay
           </Navbar.Brand>
 
-          <Navbar.Toggle />
+          <Navbar.Toggle
+            aria-controls="main-navbar"
+          />
 
-          <Navbar.Collapse>
-
+          <Navbar.Collapse id="main-navbar">
             <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/cart">
+                <FaShoppingCart className="me-1" />
+                Cart
 
-              <Nav.Link>
-
-                <FaShoppingCart /> Cart
-
+                {cartItemsCount > 0 && (
+                  <Badge
+                    pill
+                    bg="primary"
+                    className="ms-2"
+                  >
+                    {cartItemsCount}
+                  </Badge>
+                )}
               </Nav.Link>
 
-              <Nav.Link>
-
-                <FaUser /> Sign In
-
+              <Nav.Link as={Link} to="/login">
+                <FaUser className="me-1" />
+                Sign In
               </Nav.Link>
-
             </Nav>
-
           </Navbar.Collapse>
-
         </Container>
       </Navbar>
     </header>
